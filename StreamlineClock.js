3fef60c95fbc913d77614b34b0f37ad762e15a2f
Erica Bronge <erica.bronge@gmail.com>
2015-02-20 14:15:05 +0100

    Added code to read raw data

--- StreamlineClock.js
+++ StreamlineClock.js
@@ -1,1 +1,33 @@
-var raw_data;
+var data_el;
+var data_vv;
+var data_kv;
+
+var data_el_coords;
+var data_vv_coords;
+var data_kv_coords;
+
+var dsv = d3.dsv(";", "text/plain");
+
+dsv("data/30-1001-EL-short.csv", handleDataEl);
+
+function handleDataEl(data) {
+    data_el = data;
+    
+    dsv("data/30-1001-VV-short.csv", handleDataVv);
+}
+
+function handleDataVv(data) {
+    data_vv = data;
+    
+    dsv("data/30-1001-KV-short.csv", handleDataKv);
+}
+
+function handleDataKv(data) {
+    data_kv = data;
+    
+    calculateDataCoordinates();
+}
+
+function calculateDataCoordinates() {
+
+}
