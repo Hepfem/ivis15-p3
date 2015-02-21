67198dd1e0a50ebab9cd889d255f6e6dd21deb5f
Erica Bronge <erica.bronge@gmail.com>
2015-02-21 18:54:02 +0100

    Added drawing of energy usage

--- StreamlineClock.js
+++ StreamlineClock.js
@@ -6,6 +6,8 @@ var data_el_coords = [];
 var data_vv_coords = [];
 var data_kv_coords = [];
 
+var scale = 150;
+
 var dsv = d3.dsv(";", "text/plain");
 
 dsv("data/30-1001-EL-short.csv", handleDataEl);
@@ -33,6 +35,26 @@ function calculateDataCoordinates() {
         
         var angle = ((2*Math.PI)/24) * i;
         
-        data_el_coords.push({"x": Math.cos(angle) * Number(data_el[i].Energy), "y": Math.sin(angle) * Number(data_el[i].Energy)});
+        data_el_coords.push({"x": Math.cos(angle) * Number(data_el[i].Energy) * scale, "y": Math.sin(angle) * Number(data_el[i].Energy * scale)});
     }
+
+    drawClock();
 }
+
+function drawClock() {
+    var svg = d3.select("#chart").append("svg")
+                                .attr("width", 500)
+                                .attr("height", 500)
+                                .append("g")
+                                .attr("transform", "translate(250,250) rotate(-90)");;
+
+    var shape_el = svg.append("path")
+                        .attr("d", pathFunction(data_el_coords))
+                        .attr("stroke", "black")
+                        .attr("stroke-width", 1)
+                        .attr("fill", "none");
+}
+
+var pathFunction = d3.svg.line().x(function(d) { return d.x; })
+                                .y(function(d) { return d.y; })
+                                .interpolate("basis-closed");
