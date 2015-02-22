0e7eefec216cde8427864b18b5def08bb89286bc
Erica Bronge <erica.bronge@gmail.com>
2015-02-22 12:54:12 +0100

    Somewhat cleaned up coordinate calculation code

--- StreamlineClock.js
+++ StreamlineClock.js
@@ -6,7 +6,7 @@ var data_el_coords = [];
 var data_vv_coords = [];
 var data_kv_coords = [];
 
-var scale = 50;
+var scale = 40;
 var scale_v = 650;
 var offset = 120;
 var offset_v = 1;
@@ -40,19 +40,26 @@ function calculateDataCoordinates() {
         
         data_el_coords.push({"x": Math.cos(angle) * ((Number(data_el[i].Energy) * scale) + offset), "y": Math.sin(angle) * ((Number(data_el[i].Energy) * scale) + offset)});
 
-        //Determine whether to apply positive or negative offset forS water
+
+        //Water coordinates along the current angle's axis
+        var vv_value_x = (Math.cos(angle) * (Number(data_vv[i].Volume) * scale_v));
+        var vv_value_y = (Math.sin(angle) * (Number(data_vv[i].Volume) * scale_v));
+        var kv_value_x = (Math.cos(angle) * (Number(data_kv[i].Volume) * scale_v));
+        var kv_value_y = (Math.sin(angle) * (Number(data_kv[i].Volume) * scale_v));
+
+        //Determine whether to apply positive or negative offset for water
         if (i > 0 && i < 7) { //Both x and y positive
-            data_vv_coords.push({"x": data_el_coords[i].x + offset_v + (Math.cos(angle) * (Number(data_vv[i].Volume) * scale_v)), "y": data_el_coords[i].y + offset_v + (Math.sin(angle) * (Number(data_vv[i].Volume) * scale_v))});
-            data_kv_coords.push({"x": data_vv_coords[i].x + offset_v + (Math.cos(angle) * (Number(data_kv[i].Volume) * scale_v)), "y": data_vv_coords[i].y + offset_v + (Math.sin(angle) * (Number(data_kv[i].Volume) * scale_v))});
+            data_vv_coords.push({"x": data_el_coords[i].x + offset_v + vv_value_x, "y": data_el_coords[i].y + offset_v + vv_value_y});
+            data_kv_coords.push({"x": data_vv_coords[i].x + offset_v + kv_value_x, "y": data_vv_coords[i].y + offset_v + kv_value_y});
         } else if (i >= 7 && i < 13) { //Negative x, positive y
-            data_vv_coords.push({"x": data_el_coords[i].x - offset_v + (Math.cos(angle) * (Number(data_vv[i].Volume) * scale_v)), "y": data_el_coords[i].y + offset_v + (Math.sin(angle) * (Number(data_vv[i].Volume) * scale_v))});
-            data_kv_coords.push({"x": data_vv_coords[i].x - offset_v + (Math.cos(angle) * (Number(data_kv[i].Volume) * scale_v)), "y": data_vv_coords[i].y + offset_v + (Math.sin(angle) * (Number(data_kv[i].Volume) * scale_v))});
+            data_vv_coords.push({"x": data_el_coords[i].x - offset_v + vv_value_x, "y": data_el_coords[i].y + offset_v + vv_value_y});
+            data_kv_coords.push({"x": data_vv_coords[i].x - offset_v + kv_value_x, "y": data_vv_coords[i].y + offset_v + kv_value_y});
         } else if ( i >= 13 && i < 19) { //Both x and y negative
-            data_vv_coords.push({"x": data_el_coords[i].x - offset_v + (Math.cos(angle) * (Number(data_vv[i].Volume) * scale_v)), "y": data_el_coords[i].y - offset_v + (Math.sin(angle) * (Number(data_vv[i].Volume) * scale_v))});
-            data_kv_coords.push({"x": data_vv_coords[i].x - offset_v + (Math.cos(angle) * (Number(data_kv[i].Volume) * scale_v)), "y": data_vv_coords[i].y - offset_v + (Math.sin(angle) * (Number(data_kv[i].Volume) * scale_v))});
+            data_vv_coords.push({"x": data_el_coords[i].x - offset_v + vv_value_x, "y": data_el_coords[i].y - offset_v + vv_value_y});
+            data_kv_coords.push({"x": data_vv_coords[i].x - offset_v + kv_value_x, "y": data_vv_coords[i].y - offset_v + kv_value_y});
         } else { //Positive x, negative y
-            data_vv_coords.push({"x": data_el_coords[i].x + offset_v + (Math.cos(angle) * (Number(data_vv[i].Volume) * scale_v)), "y": data_el_coords[i].y - offset_v + (Math.sin(angle) * (Number(data_vv[i].Volume) * scale_v))});
-            data_kv_coords.push({"x": data_vv_coords[i].x + offset_v + (Math.cos(angle) * (Number(data_kv[i].Volume) * scale_v)), "y": data_vv_coords[i].y - offset_v + (Math.sin(angle) * (Number(data_kv[i].Volume) * scale_v))});
+            data_vv_coords.push({"x": data_el_coords[i].x + offset_v + vv_value_x, "y": data_el_coords[i].y - offset_v + vv_value_y});
+            data_kv_coords.push({"x": data_vv_coords[i].x + offset_v + kv_value_x, "y": data_vv_coords[i].y - offset_v + kv_value_y});
         }
     }
 
@@ -64,7 +71,7 @@ function drawClock() {
                                 .attr("width", 750)
                                 .attr("height", 750)
                                 .append("g")
-                                .attr("transform", "translate(250,250) rotate(-90)");;
+                                .attr("transform", "translate(250,250) rotate(-90)");
 
     var shape_kv = svg.append("path")
                         .attr("d", pathFunction(data_kv_coords))
